65228f4f45f00d2d773707a88347eca2c5b9354f
Marcos <63043499+marcosgale@users.noreply.github.com>
2025-10-16 10:05:57 -0400

    Created About
    I have developed a functional "About" page with a basic strcuture

---
+++ client/src/About.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55d5ccfdb4ef363d178ae742a194eb3a8d6dbdb8895a8f27245f5b2b467d1922
+size 1293
--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43aa37350449b13cdac814732889daf23c83e2f4b863133a96cd8b43a9925832
-size 5984
+oid sha256:08c6e6ec4f6cc46fb7d4cc0b7c45d1657ec1ac21d4cd2ce8b0f279b30998375f
+size 6068
