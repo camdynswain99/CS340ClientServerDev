74c91725f2a3c7e90800927a4de5abc7a55d506e
Jonathan Scott <jonathan.scott6@snhu.edu>
2025-10-24 10:18:04 -0400

    Attempted GitLFS removal
    - should have removed gitLFS

--- client/src/SignInComponents/SignIn.js
+++ client/src/SignInComponents/SignIn.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8cb7dabdca6b7ca9f33601dbc943515e81581ad500ea55c6d66bc910a47513a
-size 285
+oid sha256:21724d4d4b7533e95b08c7396b6b26dd824f6c1831cd3e3dbb9ef403f0276c7d
+size 2476
